00578689982ae09be16987f2fa5b8e9a6a183a50
malweis <58945171+malweis@users.noreply.github.com>
2023-05-10 09:26:53 -0400

    ahora sis

--- Ejercicios_6/Ejercicio6_1.js
+++ Ejercicios_6/Ejercicio6_1.js
@@ -1,30 +1,57 @@
-function bubbleSort(a, n, contador = 0) {
-  let sorted = true;
+function bubbleSort(a, n, primero, segundo) {
+  let pivot = 0;
+  let contador= 0 ;
   for (let i = 0; i < n - 1; i++) {
     if (a[i] < a[i + 1]) {
-      let t = a[i];
-      a[i] = a[i + 1];
-      a[i + 1] = t;
-      sorted = false;
-      contador++;
+       primero = a.slice(0, i)
+       segundo = a.slice(i)
+       pivot = a[i]
+       contador++
     }
   }
-  if (sorted == false) {
-    bubbleSort(a, n, contador);
-  } else {
-    if (contador > 1) {
-      console.log("Array ordenado : " + a.join(" "));
-    } else {
-      console.log("NiNs");
+  if(contador < 1){
+    return null
+  }
+  var mm = null, mmi = null;
+  for (var i = 0; i < segundo.length; i++) {
+    if (segundo[i] > pivot) {
+      if (mm == null || segundo[i] < mm) {
+        mm = segundo[i];
+        mmi = i;
+      }
     }
   }
+  if (mmi == null) return null;
+
+  segundo.splice(mmi, 1);
+  
+  segundo = segundo.sort();
+
+
+  var ret = +primero.concat([mm]).concat(segundo).join('');
+  console.log(ret)
+  if (ret < n) return null;
+    console.log(primero)
+    console.log(segundo)
+    console.log(pivot)
+   
+ 
 }
 
-const cadena = "3334";
+const cadena = "2034";
 const array = [];
+var result =[];
+var primero = [];
+var segundo = [];
 for (let i = 0; i < cadena.length; i++) {
   array.push(parseInt(cadena[i]));
 }
 
 let n = array.length;
-bubbleSort(array, n);
+result = bubbleSort(array, n);
+
+console.log(result);
+
+
+
+  
